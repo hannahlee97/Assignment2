1a707cdabc764f201c2fc4d05cdcb2ef35f4a09c
Haeun Lee(Hannah) <hlee405@my.bcit.ca>
2018-11-25 21:56:21 -0800

    Continue commit

--- City.cpp
+++ City.cpp
@@ -3,3 +3,22 @@
 //
 
 #include "City.hpp"
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+City::City(string name, double x, double y) : name(name), x_coordinate(x), y_coordinate(y) {}
+
+double City::distance(const City& c) {
+
+    return sqrt(pow(c.get_x() - get_x(), 2.0) + pow(c.get_y() - get_y(), 2.0));
+}
+
+bool operator==(const City & lhs, const City & rhs) {
+    if(lhs.name == rhs.name &&
+    lhs.x_coordinate == rhs.x_coordinate &&
+    lhs.y_coordinate == rhs.y_coordinate)
+        return true;
+    return false;
+}
--- City.hpp
+++ City.hpp
@@ -5,8 +5,25 @@
 #ifndef ASSIGNMENT02_CITY_HPP
 #define ASSIGNMENT02_CITY_HPP
 
+#include <algorithm>
+#include <vector>
+#include <string>
+
+using namespace std;
 
 class City {
+private:
+    string name;
+    double x_coordinate;
+    double y_coordinate;
+
+public:
+    City(string name, double x, double y);
+    string getName() { return name; }
+    double get_x() const { return x_coordinate; }
+    double get_y() const { return y_coordinate; }
+    friend bool operator==(const City & lhs, const City & rhs);
+    double distance(const City& c);
 
 };
 
